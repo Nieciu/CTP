4da32108f6de12bac49dd3f7fead1feffa8a152b
Jankowiak Patryk <https://github.com/Patrykulus/megakurs>
2022-05-26 19:22:02 +0200

    comć 3

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ fig = Figure(figsize=(6, 4), dpi=200)
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 toolbar = NavigationToolbar2Tk(canvas, root)
 photo = tkinter.PhotoImage(file=r"C:\Users\pati\PycharmProjects\CTP2\upload.png")
+t = np.arange(0, 3, .01)
+filename = fd.askopenfilename()
+raw = pd.read_csv(filename,sep = '\t',header = 0,decimal = ',') #wczytuje dane
+df = raw
+df = df.set_index(['T [s]'])
 toolbar.update()
 
 
 def init():
     root.wm_title("CTP")
-    t = np.arange(0, 3, .01)
-    filename = fd.askopenfilename()
-    df = pd.read_csv(filename, sep='\t', header=0, index_col=0, decimal=',')
     fig.add_subplot(111).plot(df)
     draw()
 
@@ -63,7 +65,21 @@ def submenu_callback():
 
 
 def antyaliasing():
-    print("antyaliasing")
+    things = raw
+    things['T [s]'] = things['T [s]'] - things.at[0, 'T [s]']
+
+    AA = {'T [s]': [things.at[0, 'T [s]']], 'Ux [V]': [things.at[0, 'Ux [V]']], 'Uy [V]': [things.at[0, 'Uy [V]']]}
+    AA = pd.DataFrame(data=AA)
+
+    for i in range(1, things['T [s]'].size):
+        var = {'T [s]': [(things.at[i - 1, 'T [s]'] + things.at[i, 'T [s]']) / 2, things.at[i, 'T [s]']],
+               'Ux [V]': [((things.at[i - 1, 'Ux [V]']) + (things.at[i, 'Ux [V]'])) / 2, things.at[i, 'Ux [V]']],
+               'Uy [V]': [((things.at[i - 1, 'Uy [V]']) + (things.at[i, 'Uy [V]'])) / 2, things.at[i, 'Uy [V]']]}
+        var = pd.DataFrame(data=var)
+        AA = pd.concat([AA, var], ignore_index=True)
+    df = AA
+    fig.clear()
+    init()
 
 
 def open_calc_widnow():
